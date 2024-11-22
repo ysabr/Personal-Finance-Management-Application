ba090f63e7909caea009b1fdd2e08b3af98adaaa
Youssef SABR <youssef.sabr@um5r.ac.ma>
2024-11-22 15:50:21 +0100

    install npm for front end and add configuration of Rest fraework

--- backend/settings.py
+++ backend/settings.py
@@ -83,14 +83,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'finance_db',
-        'USER': 'postgres',
-        'PASSWORD': 'yourpassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 
 
 
